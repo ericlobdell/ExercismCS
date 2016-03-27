0883a45e8372a0a4a49b6dc8ba6b3423c0d4a7d4
Eric Lobdell <eric@ericlobdell.me>
2016-03-27 17:27:40 -0400

    finished Anagram

---
+++ ExercismCS/Anagram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExercismCS
+{
+    public class Anagram
+    {
+        private string _control;
+        private string _original;
+
+        public Anagram( string original )
+        {
+            _original = original;
+            _control = Normalize( original );
+        }
+
+        public IEnumerable<string> Match( IEnumerable<string> words )
+        {
+            return words
+                .Where( w => Normalize( w ) == _control
+                          && w.ToLower( ) != _original.ToLower( ) );
+        }
+
+        private string Normalize( string s )
+        {
+            return string.Concat(
+                    s
+                    .ToLower( )
+                    .OrderBy( c => c )
+                );
+        }
+    }
+}
