4ad0fd091637cf10eef50a8caf6ecf5f7209163f
Eric Lobdell <eric@ericlobdell.me>
2016-03-27 16:04:33 -0400

    finished ETL

---
+++ ExercismCS/ETL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercismCS
+{
+    public static class ETL
+    {
+        public static Dictionary<string, int> Transform( Dictionary<int, IList<string>> data )
+        {
+            var result = new Dictionary<string, int>( );
+
+            foreach ( var record in data )
+            {
+                foreach ( var letter in record.Value )
+                {
+                    result.Add( letter.ToLower(), record.Key );
+                }
+            }
+
+            return result;
+        }
+    }
+}
+ 
