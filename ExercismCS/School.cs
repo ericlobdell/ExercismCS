95c2937d5f7825cc7a898012b79a8f317508b01f
Eric Lobdell <eric@ericlobdell.me>
2016-03-28 18:55:02 -0400

    finished grade school

---
+++ ExercismCS/School.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercismCS
+{
+    public class School
+    {
+        public Dictionary<int, List<string>> Roster { get; set; }
+
+        public School( )
+        {
+            Roster = new Dictionary<int, List<string>>( );
+        }
+
+        public void Add( string name, int grade )
+        {
+            if ( Roster.ContainsKey( grade ) )
+            {
+                Roster [ grade ].Add( name );
+                Roster [ grade ].Sort( );
+            }
+            else
+                Roster.Add( grade, new List<string> { name } );
+        }
+
+        public List<string> Grade( int grade )
+        {
+            if ( Roster.ContainsKey( grade ) )
+                return Roster [ grade ];
+            else
+                return new List<string>( );
+        }
+    }
+}
