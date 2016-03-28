044171d191d0fd5b4e56aa113bcd4dcf6cb19254
Eric Lobdell <eric@ericlobdell.me>
2016-03-28 13:00:30 -0400

    finished Hamming

---
+++ ExercismCS/Hamming.cs
@@ -0,0 +1,17 @@
+﻿namespace ExercismCS
+{
+    public static class Hamming
+    {
+        public static int Compute( string strand1, string strand2 )
+        {
+            var distance = 0;
+            for ( int i = 0; i < strand1.Length; i++ )
+            {
+                if ( strand1 [ i ] != strand2 [ i ] )
+                    distance++;
+            }
+
+            return distance;
+        }
+    }
+}
