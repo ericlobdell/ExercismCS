32b11abbdea404f79e67d4babc07a183a2952c47
Eric Lobdell <eric@ericlobdell.me>
2016-03-29 11:05:22 -0400

    refactoring sum of multiples

--- ExercismCS/SumOfMultiples.cs
+++ ExercismCS/SumOfMultiples.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExercismCS
 {
@@ -10,19 +7,19 @@ namespace ExercismCS
     {
         private static List<int> multiples;
 
-        public static int To( int[] testMultiples, int num)
+        public static int To( int [] testMultiples, int num )
         {
             multiples = new List<int>( );
 
             foreach ( var test in testMultiples )
-                for ( int i = 0; i < num; i++ )
-                    if ( IsMultipleOf( i, test ) && !multiples.Contains(i) )
+                for ( var i = 0; i < num; i++ )
+                    if ( IsMultipleOf( i, test ) )
                         multiples.Add( i );
-               
-            return multiples.Sum( );
+
+            return multiples.Distinct().Sum( );
         }
 
-        private static bool IsMultipleOf( int num, int test)
+        private static bool IsMultipleOf( int num, int test )
         {
             return num % test == 0;
         }
