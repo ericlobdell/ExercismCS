b3d984c0d88e564e594ca118ba88133b5df52445
Eric Lobdell <eric@ericlobdell.me>
2016-04-02 09:59:47 -0400

    finished gigasecond

---
+++ ExercismCS/Gigasecond.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercismCS
+{
+    public static class Gigasecond
+    {
+        public static DateTime Date( DateTime birthDate )
+        {
+            return birthDate.AddSeconds( Math.Pow( 10.0, 9.0 ) );
+        }
+    }
+}
