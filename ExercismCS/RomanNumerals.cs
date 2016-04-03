22e206a3e07ac0e9b4721e4e4ef9679ad4671106
Eric Lobdell <eric@ericlobdell.me>
2016-04-03 11:04:39 -0400

    refactored roman numerals

--- ExercismCS/RomanNumerals.cs
+++ ExercismCS/RomanNumerals.cs
@@ -16,88 +16,24 @@ namespace ExercismCS
 
         private static string ConvertNumber( int number )
         {
+            var RomanNumeralMap = new Dictionary<string, int>( )
+            {
+                { "M", 1000 }, { "CM", 900 }, { "D", 500 }, {  "CD", 400 }, { "C", 100 }, { "XC", 90 },
+                {  "L", 50 }, { "XL", 40 }, { "X", 10 }, { "IX", 9 }, { "V", 5 } { "IV", 4 }, { "I", 1 }
+            };
+
             var result = "";
 
-            if (number > 0)
+            if ( number > 0 )
             {
-                while (number >= 1000)
+                foreach ( var mapping in RomanNumeralMap )
                 {
-                    result += "M";
-                    number -= 1000;
+                    while ( number >= mapping.Value )
+                    {
+                        result += mapping.Key;
+                        number -= mapping.Value;
+                    }
                 }
-
-                if ( number >= 900 )
-                {
-                    result += "CM";
-                    number -= 900;
-                }
-
-                if ( number >= 500 )
-                {
-                    result += "D";
-                    number -= 500;
-                }
-
-                if ( number >= 400 )
-                {
-                    result += "CD";
-                    number -= 400;
-                }
-
-                while ( number >= 100 )
-                {
-                    result += "C";
-                    number -= 100;
-                }
-
-                if ( number >= 90 )
-                {
-                    result += "XC";
-                    number -= 90;
-                }
-
-                if (number >= 50)
-                {
-                    result += "L";
-                    number -= 50;
-                }
-
-                if ( number >= 40 )
-                {
-                    result += "XL";
-                    number -= 40;
-                }
-
-                while ( number >= 10 )
-                {
-                    result += "X";
-                    number -= 10;
-                }
-
-                if ( number == 9 )
-                {
-                    result += "IX";
-                    number = 0;
-                }
-
-                if ( number >= 5 )
-                {
-                    result += "V";
-                    number -= 5;
-                }
-
-                if ( number == 4 )
-                {
-                    result += "IV";
-                    number = 0;
-                }
-
-                while ( number > 0 )
-                {
-                    result += "I";
-                    number -= 1;
-                }
-
             }
 
             return result;
