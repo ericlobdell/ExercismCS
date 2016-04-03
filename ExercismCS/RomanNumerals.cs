721f53092b7088ca9c626f274b28bccf62a8e140
Eric Lobdell <eric@ericlobdell.me>
2016-04-03 11:08:08 -0400

    refactoring

--- ExercismCS/RomanNumerals.cs
+++ ExercismCS/RomanNumerals.cs
@@ -8,20 +8,19 @@ namespace ExercismCS
 {
     public static class RomanNumerals
     {
+        private static Dictionary<string, int> RomanNumeralMap = new Dictionary<string, int>( )
+            {
+                { "M", 1000 }, { "CM", 900 }, { "D", 500 }, {  "CD", 400 }, { "C", 100 }, { "XC", 90 },
+                {  "L", 50 }, { "XL", 40 }, { "X", 10 }, { "IX", 9 }, { "V", 5 }, { "IV", 4 }, { "I", 1 }
+            };
+
         public static string ToRoman( this int number )
         {
-
             return ConvertNumber( number );
         }
 
         private static string ConvertNumber( int number )
         {
-            var RomanNumeralMap = new Dictionary<string, int>( )
-            {
-                { "M", 1000 }, { "CM", 900 }, { "D", 500 }, {  "CD", 400 }, { "C", 100 }, { "XC", 90 },
-                {  "L", 50 }, { "XL", 40 }, { "X", 10 }, { "IX", 9 }, { "V", 5 } { "IV", 4 }, { "I", 1 }
-            };
-
             var result = "";
 
             if ( number > 0 )
